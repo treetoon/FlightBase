81904a539f261db634bcd318b5f831d8beedeba7
Daniel Johansson <danielj@comhem.se>
2018-03-20 10:42:44 +0100

    Additions to Airplane class

--- src/se/lexicon/application/Main.java
+++ src/se/lexicon/application/Main.java
@@ -1,13 +1,18 @@
 package se.lexicon.application;
 
+import se.lexicon.model.airline.Airplane;
+import se.lexicon.model.airline.types.SectionType;
 import se.lexicon.ui.SystemUI;
 
 public class Main {
 
     public static void main(String[] args) {
+        Airplane a=new Airplane("flightnret", 203);
+        //a.reserveSeat(SectionType.BUSINESS);
 
-        SystemUI ui = new SystemUI();
-        ui.start();
+
+        //SystemUI ui = new SystemUI();
+        //ui.start();
 
     }
 }
--- src/se/lexicon/model/airline/Airplane.java
+++ src/se/lexicon/model/airline/Airplane.java
@@ -1,18 +1,127 @@
 package se.lexicon.model.airline;
 
+import se.lexicon.model.airline.types.SectionType;
+
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class Airplane {
 
     private String flightNr;
-    Map<String, Boolean> businessSeatList;
-    Map<String, Boolean> economySeatList;
 
-    public void reserveSeat( ) {
-        
+    private Map<String, Boolean> businessSeatList;
+    private Map<String, Boolean> economySeatList;
+
+    public Airplane(String flightNr, int size) {
+        this.flightNr=flightNr;
+
+        businessSeatList=new TreeMap<>();
+        economySeatList=new TreeMap<>();
+
+        //6 platser per rad (A-F)
+        //A-C == BUSINESS
+        //D-F == ECONOMY
+
+        int numberOfRows=size/6;
+        int remainingSeats=size%6;
+
+        System.out.println("rows: " + numberOfRows);
+        System.out.println("resten: " + remainingSeats);
+
+        for (int i=1; i<=numberOfRows; i++) {
+            businessSeatList.put(i+ "A", false);
+            businessSeatList.put(i+ "B", false);
+            businessSeatList.put(i+ "C", false);
+
+            economySeatList.put(i+ "D", false);
+            economySeatList.put(i+ "E", false);
+            economySeatList.put(i+ "F", false);
+
+        }
+
+        //If there are remaining seats to be assigned (a last row)
+        if (remainingSeats > 0) {
+            char seatLetter = 'A';
+            int lastRowNumber = numberOfRows + 1;
+
+            for (int i = 1; i <= remainingSeats; i++) {
+                if (seatLetter == 'A' || seatLetter == 'B' | seatLetter == 'C') {
+                    String s = "" + lastRowNumber + seatLetter;
+                    //System.out.println("business " + s);
+                    businessSeatList.put(s, false);
+                } else {
+                    String s = "" + lastRowNumber + seatLetter;
+                    //System.out.println("economy" + s);
+                    economySeatList.put(s, false);
+                }
+                seatLetter++;
+            }
+        }
+
+//        System.out.println("Businesslistan: ");
+//        for (Map.Entry<String, Boolean> entry : businessSeatList.entrySet()) {
+//            System.out.println("Key : " + entry.getKey() + " Value : " + entry.getValue());
+//        }
+//
+//        System.out.println("Economylistan: ");
+//        for (Map.Entry<String, Boolean> entry : economySeatList.entrySet()) {
+//            System.out.println("Key : " + entry.getKey() + " Value : " + entry.getValue());
+//        }
+
+
     }
 
-    public void unreserveSeat() {
+    public String reserveSeat(SectionType type) {
+
+        if (type==SectionType.BUSINESS) {
+            Iterator<Map.Entry<String, Boolean>> entries = businessSeatList.entrySet().iterator();
+            while (entries.hasNext()) {
+                Map.Entry<String, Boolean> entry = entries.next();
+
+                // if seat is empty (false), set to true
+                if (entry.getValue()==false) {
+                    entry.setValue(true);
+                    return entry.getKey();
+
+                }
+            }
+        }
+        else if (type==SectionType.ECONOMY) {
+            Iterator<Map.Entry<String, Boolean>> entries = economySeatList.entrySet().iterator();
+            while (entries.hasNext()) {
+                Map.Entry<String, Boolean> entry = entries.next();
+
+                if (entry.getValue()==false) {
+                    entry.setValue(true);
+                    return entry.getKey();
+                }
+            }
+        }
+
+        return null;
+    }
+
+    //kan SectionType vara med som parameter?
+    public void unreserveSeat(String seat) {
+        Iterator<Map.Entry<String, Boolean>> entries = businessSeatList.entrySet().iterator();
+        while (entries.hasNext()) {
+            Map.Entry<String, Boolean> entry = entries.next();
+
+            if (entry.getKey().equals(seat)) {
+                entry.setValue(false);
+            }
+        }
+
+        entries = economySeatList.entrySet().iterator();
+        while (entries.hasNext()) {
+            Map.Entry<String, Boolean> entry = entries.next();
+
+            if (entry.getKey().equals(seat)) {
+                entry.setValue(false);
+            }
+        }
 
     }
 
